8e35cfe7c66b27c5b8cf8f2e2604b2ed216e7c08
jgaye <pi@raspberrypi.(none)>
2014-08-19 21:08:42 +0000

    [#14] Working version of box + Wrapped the lines + change name of the file + stop following the test files + test executed + Changed the representations to non unicode

---
+++ src/box.py
@@ -0,0 +1,47 @@
+class Box(object):
+    """
+    A Box is an object that have one of these three status: unknown (-1), white
+    (0), black (1).  It is initiated to unknown and can be then filled to white
+    or black once.  Once it has been filled, it can't change status anymore.  A
+    Box is printed as a ? (unknown), a white square (white) or a black square
+    (black).
+    """
+    #Init to unknown
+
+    def __init__(self):
+        self.value = -1
+
+    #Status setters
+
+    def fill_white(self):
+        if self.is_unknown():
+            self.value = 0
+        else:
+            print "This Box has already been filled"
+
+    def fill_black(self):
+        if self.is_unknown():
+            self.value = 1
+        else:
+            print "This Box has already been filled"
+
+    #Status getters
+
+    def is_unknown(self):
+        return self.value == -1
+
+    def is_white(self):
+        return self.value == 0
+
+    def is_black(self):
+        return self.value == 1
+
+    #Representation
+
+    def __repr__(self):
+        if self.is_unknown():
+            return "[?]"
+        elif self.is_white():
+            return "[ ]"
+        else:
+            return "[X]"
